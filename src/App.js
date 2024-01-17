ab6c3a4cbf906572c28e21eeb800941fc900d348
fernando leon <ferleonqaq@gmail.com>
2024-01-17 01:39:47 -0400

    turnos en tablero

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './App.css'
 const TURNS = {
   X: 'x',
@@ -5,15 +6,31 @@ const TURNS = {
 }
 
 
- const Square=({children,updateBoard,index})=>{
+ const Square=({children,isSelected,updateBoard,index})=>{
+  const className = `square ${isSelected ? 'is-selected' : ''}`
+
+  const handleClick = () =>{
+    updateBoard(index)
+  }
+
   return(
-    <div className="square">
+    <div onClick={handleClick} className={className}>
       {children}
     </div>
   )
  }
 function App() {
-  const board=Array(9).fill(null)
+  const [board,setBoard]=useState(Array(9).fill(null))
+  const [turn,setTurn]=useState(TURNS.X)
+
+  const updateBoard = (index) => { 
+    const newBoard=[...board]
+    newBoard[index] =turn
+    setBoard(newBoard)
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+  }
+
   return (
     <main className="container">
       <h1>TRES EN RAYA</h1>
@@ -24,12 +41,21 @@ function App() {
               <Square
                 key={index}
                 index={index}
+                updateBoard={updateBoard}
               >
-                
+                {board[index]}
               </Square>  
             )
           })
         }
+      </section>
+      <section className='turn'>
+        <Square isSelected={turn ===TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn=== TURNS.O}>
+          {TURNS.O}
+        </Square>
       </section>
     </main>
   );
