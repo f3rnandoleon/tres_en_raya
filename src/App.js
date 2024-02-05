5df90b313be43b7ee47b7309a2702e79c93958d7
fernando leon <ferleonqaq@gmail.com>
2024-02-04 23:42:15 -0400

    actualizdo

--- src/App.js
+++ src/App.js
@@ -6,8 +6,16 @@ import {TURNS,WINNER_COMBOS} from './constantes'
 import { WinnerBox } from './componentes/WinnerBox'
 import { checkWinner,checkEndGame } from './logic/functions'
 function App() {
-  const [board,setBoard]=useState(Array(9).fill(null))
-  const [turn,setTurn]=useState(TURNS.X)
+  const [board,setBoard]=useState(()=>{
+    const boardFromStorage=window.localStorage.getItem('board')
+    if(boardFromStorage) return JSON.parse(boardFromStorage)
+    return Array(9).fill(null)
+  })
+  const [turn,setTurn]=useState(()=>{
+    const turnFromStorage=window.localStorage.getItem('turn')
+    if(turnFromStorage) return turnFromStorage
+    return TURNS.X
+  })
   const [winner,seTWinner]=useState(null)
 
  
@@ -15,6 +23,8 @@ function App() {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     seTWinner(null)
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
   }
 
   
@@ -28,8 +38,8 @@ function App() {
 
       const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
       setTurn(newTurn) 
-      window.localStorage.setItem('board',JSON.stringify(board))
-      window.localStorage.setItem('turn',turn)
+      window.localStorage.setItem('board',JSON.stringify(newBoard))
+      window.localStorage.setItem('turn',newTurn)
       const newWinner= checkWinner(newBoard)
       if(newWinner){   
         confetti()   
